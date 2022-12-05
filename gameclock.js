229b8def0703f695ad8a85bf42763af49166a04a
Motoshi0711 <motoshi.suzuki.hp@gmail.com>
2022-12-05 21:18:29 +0900

    between

--- gameclock.js
+++ gameclock.js
@@ -1,103 +1,141 @@
-var Clock = function(config) {
-    var clock = this;
-    var time = config.time,
-        remainingTime = time,
-        countDown=true,
-        qClock, minutes = 0,
-        resetAfterTimeout = true,
-        stopAferTimeout = true,
-        seconds = 0,
-        timeoutCallBack, 
-        on = false,
-        resume = true;
-    assignUserSettings();
-    displayClock();
 
-    this.SetTime = function(newTime) {
-        remainingTime = newTime;
-        time = newTime;
-        displayClock();
-    }
+let playing = false;
+let currentPlayer = 1;
+const timerPanel = document.querySelector('.player');
+const buttons = document.querySelectorAll('.bttn');
+// Sound effects for project.
+// const timesUp = new Audio('audio/460133__eschwabe3__robot-affirmative.wav');
+// const click = new Audio('audio/561660__mattruthsound.wav');
 
-    function assignUserSettings() {
-        if (config.resetAfterTimeout != null) {
-            resetAfterTimeout = config.resetAfterTimeout;
-        }
-        if (config.stopAferTimeout != null) {
-            stopAferTimeout = config.stopAferTimeout;
-        }
-        if (config.timeoutCallBack != null) {
-            timeoutCallBack = config.timeoutCallBack;
-        }
-        if(config.time===0){
-            countDown=false;
-            topAferTimeout = false;
-        }
-    }
 
-    function calculateTime() {
-        if (resume === true) {
-            if(countDown===true){
-                if (remainingTime <= 0) {
-                    if (resetAfterTimeout === true) {
-                        clock.ResetClock();
-                        resume = true;
-                    }
-                    if (stopAferTimeout === true) {
-                        clock.StopClock();
-                        resume = false;
-                    }
+// Add a leading zero to numbers less than 10.
+const padZero = (number) => {
+    if (number < 10) {
+        return '0' + number;
+    }
+    return number;
+}
 
-                    if (timeoutCallBack != null) {
-                        timeoutCallBack();
-                    }
-                }
-            }
 
-            if (resume === true) {
-                if(countDown===true){
-                    remainingTime = remainingTime - 1000;
-                }
-                else{
-                    remainingTime = remainingTime + 1000;
-                }					
-                displayClock();
-            }
-        }
+// Create a class for the timer.
+class Timer {
+    constructor(player, minutes) {
+        this.player = player;
+        this.minutes = minutes;
     }
-    //The actual calculation that converts the milliseconds to a mm:ss  and assigns to DOM
-    function displayClock() {
-        minutes = Math.floor(remainingTime / 1000 / 60);
-        seconds = Math.floor(remainingTime / 1000) % 60;
-        var displaySec = seconds;
-        if (seconds < 10) {
-            displaySec = '0' + seconds;
-        }
-        if (config.displayEL != null) {
-            document.getElementById(config.displayEL).innerHTML = minutes + ":" + displaySec;
-        }
-
-    }
-    //Starts the clock
-    this.StartClock = function() {
-        resume = true;
-        if (on === false) {
-            qClock = setInterval(function() {
-                on = true;
-                calculateTime();
-            }, 1000);
-        }
-    }
-    //stops the clock
-    this.StopClock = function() {
-        on = false;
-        clearInterval(qClock)
-    }
-    //resets the clock
-    this.ResetClock = function() {
-        remainingTime = time;
-        qClock, minutes = 0;
-        seconds = 0;
-        displayClock();
+    getMinutes(timeId) {
+        return document.getElementById(timeId).textContent;
     }
 }
+
+let p1time = new Timer('min1', document.getElementById('min1').textContent);
+let p2time = new Timer('min2', document.getElementById('min2').textContent);
+
+
+// Swap player's timer after a move (player1 = 1, player2 = 2).
+const swapPlayer = () => {
+    if (!playing) return;
+    // Toggle the current player.
+    currentPlayer = currentPlayer === 1 ? 2 : 1;
+    // Play the click sound.
+    // click.play();
+}
+
+
+// Warn player if time drops below thirty seconds.
+const timeWarning = (player, min, sec) => {
+    // Change the numbers to red during the last 30 seconds.
+    if (min < 1 && sec <= 30) {
+        if (player === 1) {
+            document.querySelector('.player-1 .player__digits').style.color = '#CC0000';
+        } else {
+            document.querySelector('.player-2 .player__digits').style.color = '#CC0000';
+        }
+    }
+}
+
+
+// Start timer countdown to zero.
+const startTimer = () => {
+    playing = true;
+    let p1sec = 60;
+    let p2sec = 60;
+
+    let timerId = setInterval(function() {
+        // Player 1.
+        if (currentPlayer === 1) {
+            if (playing) {
+                // Disable start button.
+                buttons[0].disabled = true;
+                p1time.minutes = parseInt(p1time.getMinutes('min1'), 10);
+                if (p1sec === 60) {
+                    p1time.minutes = p1time.minutes - 1;
+                }
+                p1sec = p1sec - 1;
+                timeWarning(currentPlayer, p1time.minutes, p1sec);
+                document.getElementById('sec1').textContent = padZero(p1sec);
+                document.getElementById('min1').textContent = padZero(p1time.minutes);
+                if (p1sec === 0) {
+                    // If minutes and seconds are zero stop timer with the clearInterval method.
+                    if (p1sec === 0 && p1time.minutes === 0) {
+                        // Play a sound effect.
+                        // timesUp.play();
+                        // Stop timer.
+                        clearInterval(timerId);
+                        playing = false;
+                    }
+                    p1sec = 60;
+                }
+            }
+        } else {
+        // Player 2.
+            if (playing) {
+                p2time.minutes = parseInt(p2time.getMinutes('min2'), 10);
+                if (p2sec === 60) {
+                    p2time.minutes = p2time.minutes - 1;
+                }
+                p2sec = p2sec - 1;
+                timeWarning(currentPlayer, p2time.minutes, p2sec);
+                document.getElementById('sec2').textContent = padZero(p2sec);
+                document.getElementById('min2').textContent = padZero(p2time.minutes);
+                if (p2sec === 0) {
+                    // If minutes and seconds are zero stop timer with the clearInterval method.
+                    if (p2sec === 0 && p2time.minutes === 0) {
+                        // Play a sound effect.
+                        // timesUp.play();
+                        // Stop timer.
+                        clearInterval(timerId);
+                        playing = false;
+                    }
+                    p2sec = 60;
+                }
+            }
+        }
+    }, 1000);
+}
+
+
+// Listen for a mouse click or tap on the screen to toggle between timers.
+timerPanel.addEventListener('click', swapPlayer);
+
+// Loop through the start and reset buttons.
+for (let i = 0; i < buttons.length; i++) {
+    buttons[i].addEventListener('click', () => {
+        if (buttons[i].textContent === 'START') {
+            // Turn the button a gray color to signify a disabled button.
+            buttons[i].style.color = '#EEEEEE';
+            buttons[i].style.backgroundColor = '#606060';
+            startTimer();
+        } else {
+            // Reset everything by reloading the page.
+            location.reload(true);
+        }
+    });
+}
+
+// Listen for the press of the spacebar.
+document.addEventListener('keypress', event => {
+    if (event.keyCode === 32 || event.which === 32) {
+        swapPlayer();
+    }
+});
